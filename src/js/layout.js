335c2dd822f322d79a4cc4d26fc7ccce920e3325
George Mihov <gmihov001@gmail.com>
2022-02-15 17:35:39 +0000

    Initial

--- src/js/layout.js
+++ src/js/layout.js
@@ -10,33 +10,35 @@ import injectContext from "./store/appContext";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 import { Card } from "./component/card";
+import { Planets } from "./views/planets";
 
 //create your first component
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-	const basename = process.env.BASENAME || "";
+  //the basename is used when your project is published in a subdirectory and not in the root of the domain
+  // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+  const basename = process.env.BASENAME || "";
 
-	return (
-		<div>
-			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Navbar />
-					<Switch>
-						<Route exact path="/" component={Home}/>
-						<Route exact path="/demo" component={Demo}/>
-						<Route exact path="/single/:theid" component={Single}/>
-							
-						<Route>
-							<h1>Not found!</h1>
-						</Route>
-					</Switch>
-					<Card />
-					<Footer />
-				</ScrollToTop>
-			</BrowserRouter>
-		</div>
-	);
+  return (
+    <div>
+      <BrowserRouter basename={basename}>
+        <ScrollToTop>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/demo" component={Demo} />
+            <Route exact path="/single/:theid" component={Single} />
+			<Route exact path="/planets" component={Planets} />
+
+            <Route>
+              <h1>Not found!</h1>
+            </Route>
+          </Switch>
+
+          <Footer />
+        </ScrollToTop>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default injectContext(Layout);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+  // const [planetsArray, setPlanetsArray] = useState([]);
+  const [charactersArray, setCharactersArray] = useState([]);
+ 
+
+  const getCharacters = (params) => {
+    fetch("https://swapi.dev/api/people/")
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        // Read the response as json.
+        const data = response.json();
+        return data;
+      })
+      .then((responseAsJson) => {
+        // Do stuff with the JSONified response
+        console.log(responseAsJson);
+      })
+      .catch((error) => {
+        console.log("Looks like there was a problem: \n", error);
+      });
+  };
+  useEffect(() => {
+    getCharacters();
+    //  getPlanets();
+  }, []);
+  //   	const response = fetch("https://swapi.dev/api/people/");
+  //     	const data =  response.json();
+  //       setCharactersArray(data.results);
+  //       console.log(data);
+  // };
+  // const getPlanets = (params) => {
+  //   	const response =  fetch("https://swapi.dev/api/planets/");
+  //     	const data =  response.json();
+  //       setPlanetsArray(data.results);
+  //       console.log(data);
+
+  return (
+    <div className="text-center mt-5">
+      <h1>Hello Rigo!</h1>
+      <p>
+        <img src={rigoImage} />
+      </p>
+      <a href="#" className="btn btn-success">
+        If you see this green button, bootstrap is working
+      </a>
+    </div>
+  );
+};
