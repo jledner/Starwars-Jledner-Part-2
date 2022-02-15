b7fa0159d3800780f60c86e482ee59c13d586147
George Mihov <gmihov001@gmail.com>
2022-02-15 17:49:58 +0000

    Solved

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,43 +3,34 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-  // const [planetsArray, setPlanetsArray] = useState([]);
+  const [planetsArray, setPlanetsArray] = useState([]);
   const [charactersArray, setCharactersArray] = useState([]);
  
 
-  const getCharacters = (params) => {
-    fetch("https://swapi.dev/api/people/")
+  const getData = (url, setArray) => {
+    fetch(url)
       .then((response) => {
         if (!response.ok) {
           throw Error(response.statusText);
         }
         // Read the response as json.
-        const data = response.json();
-        return data;
+        return response.json();
+
       })
       .then((responseAsJson) => {
         // Do stuff with the JSONified response
-        console.log(responseAsJson);
+        setArray(responseAsJson.results);
       })
       .catch((error) => {
         console.log("Looks like there was a problem: \n", error);
       });
   };
-  useEffect(() => {
-    getCharacters();
-    //  getPlanets();
-  }, []);
-  //   	const response = fetch("https://swapi.dev/api/people/");
-  //     	const data =  response.json();
-  //       setCharactersArray(data.results);
-  //       console.log(data);
-  // };
-  // const getPlanets = (params) => {
-  //   	const response =  fetch("https://swapi.dev/api/planets/");
-  //     	const data =  response.json();
-  //       setPlanetsArray(data.results);
-  //       console.log(data);
 
+  useEffect(() => {
+    getData("https://swapi.dev/api/people/", setCharactersArray);
+    getData("https://swapi.dev/api/planets/", setPlanetsArray);
+  }, []);
+  
   return (
     <div className="text-center mt-5">
       <h1>Hello Rigo!</h1>
